4e5b905a76a8d163ce333b9f4e2baa96d70f9622
MakerDoe <109215379+MakerDoe@users.noreply.github.com>
2024-04-14 03:04:14 +0800

    Update GUI button positions and add new button functionality

--- src/StarterGui/ExampleGui/Parent2/Parent1/ExampleFrame/example.client.lua
+++ src/StarterGui/ExampleGui/Parent2/Parent1/ExampleFrame/example.client.lua
@@ -10,11 +10,26 @@ local Parent1 = ExampleFrame.Parent
 local Parent2 = Parent1.Parent
 local screenGui = ExampleFrame:FindFirstAncestorOfClass("ScreenGui")
 
+local insideFrame = ExampleFrame:FindFirstChild("InsideFrame")
+local insideFrameViewer = Parent1:FindFirstChild("InsideFrameViewer")
+
 local button1 = screenGui:FindFirstChild("Button1")
 local button2 = screenGui:FindFirstChild("Button2")
 local button3 = screenGui:FindFirstChild("Button3")
+local button4 = screenGui:FindFirstChild("Button4")
 
 local DraggableClass = Draggable.new(ExampleFrame)
+
+local function UpdateInsideFrameViewerPosition()
+	local insideFramePosition = insideFrame.AbsolutePosition
+	local parent1Position = Parent1.AbsolutePosition
+
+	local difference = insideFramePosition - parent1Position
+
+	local newPosition = insideFrameViewer.AbsoluteSize * insideFrameViewer.AnchorPoint + difference
+
+	insideFrameViewer.Position = UDim2.fromOffset(newPosition.X, newPosition.Y)
+end
 
 DraggableClass.Began:Connect(function(mousePosition)
 	print(`Began: {mousePosition}`)
@@ -26,6 +41,10 @@ end)
 
 DraggableClass.Ended:Connect(function(mousePosition)
 	print(`Ended: {mousePosition}`)
+
+	if DraggableClass.Boundary == insideFrame then
+		UpdateInsideFrameViewerPosition()
+	end
 end)
 
 button1.MouseButton1Click:Connect(function()
@@ -33,6 +52,7 @@ button1.MouseButton1Click:Connect(function()
 
 	Parent1.BackgroundTransparency = 1
 	Parent2.BackgroundTransparency = 1
+	insideFrameViewer.BackgroundTransparency = 1
 end)
 
 button2.MouseButton1Click:Connect(function()
@@ -40,6 +60,7 @@ button2.MouseButton1Click:Connect(function()
 
 	Parent2.BackgroundTransparency = 0
 	Parent1.BackgroundTransparency = 1
+	insideFrameViewer.BackgroundTransparency = 1
 end)
 
 button3.MouseButton1Click:Connect(function()
@@ -47,4 +68,15 @@ button3.MouseButton1Click:Connect(function()
 
 	Parent1.BackgroundTransparency = 0
 	Parent2.BackgroundTransparency = 1
+	insideFrameViewer.BackgroundTransparency = 1
 end)
+
+button4.MouseButton1Click:Connect(function()
+	UpdateInsideFrameViewerPosition()
+
+	DraggableClass.Boundary = insideFrame
+
+	insideFrameViewer.BackgroundTransparency = 0
+	Parent1.BackgroundTransparency = 1
+	Parent2.BackgroundTransparency = 1
+end) 
