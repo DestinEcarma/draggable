4e541294794dcf52bf6beba3b7aa8e9820f513a3
Destin Ecarma <109215379+DestinEcarma@users.noreply.github.com>
2024-08-23 02:03:43 +0800

    Delete Packages directory

--- Packages/Signal.lua
+++
@@ -1,6 +0,0 @@
-local fastsignal = require(script.Parent._Index["lucasmzreal_fastsignal@10.2.1"]["fastsignal"])
-
-export type Class = fastsignal.Class
-export type ScriptConnection = fastsignal.ScriptConnection
-
-return fastsignal
